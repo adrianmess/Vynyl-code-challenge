f297e5f080d47b209e9be035902108d1fa29e56a
Adrian Messado <adrian.messado@gmail.com>
2020-01-02 23:55:18 -0800

    add nestesdARray variable, add RecursiveFlat function, write expect statement set to equal expected output. confirm npm test passes

--- spec/tests.js
+++ spec/tests.js
@@ -1,6 +1,16 @@
 
 describe("Flatten Array", function() {
   it("should test whether the nested array is converted to flat array", function() {
+	const nestedArray = [["one", "two", ["three"]], "four"];
+
+    function RecursiveFlat(nestedArray) {
+      return nestedArray.reduce(
+        (acc, val) =>
+          Array.isArray(val) ? acc.concat(RecursiveFlat(val)) : acc.concat(val),
+        []
+      );
+    }
+    expect(RecursiveFlat(nestedArray)).toEqual(["one", "two", "three", "four"]);
   });
 });
 
