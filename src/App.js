21dfa1538b9e5b1c44bb32c0a2d2b6b5fbe9a3a7
Adrian Messado <adrian.messado@gmail.com>
2020-01-02 23:09:51 -0800

    add code challenge instructions

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <p>
+          <b>Instructions:</b> Write some code to flatten an array of
+          arbitrarily nested arrays of strings into a flat array of strings
+          (e.g., [['one','two',['three']],'four'] ->
+          ['one','two','three','four'].
+        </p>
       </div>
     );
   }
